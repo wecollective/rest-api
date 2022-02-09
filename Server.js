2566d8858894682ab980ea740c4e773624665f61
jhweir <james@weco.io>
2022-02-09 14:55:39 +0000

    Signal names updated, new topic image signal added

--- Server.js
+++ Server.js
@@ -59,9 +59,8 @@ const socketsToRooms = []
 const maxPlayers = 10
 
 io.on('connection', socket => {
-    socket.on('join-room', data => {
+    socket.on('outgoing-join-room', data => {
         const { roomId, userData } = data
-        console.log(data)
         // create user object
         const user = { socketId: socket.id, userData }
         // if no room, create room
@@ -73,32 +72,31 @@ io.on('connection', socket => {
         // connect to room
         socket.join(roomId)
         // notify room of new user
-        socket.to(roomId).emit('user-joined', user)
+        socket.to(roomId).emit('incoming-user-joined', user)
         // send room data back to new user
         const usersInRoom = rooms[roomId].filter(user => user.socketId !== socket.id)
-        socket.emit('room-joined', { socketId: socket.id, usersInRoom })
+        socket.emit('incoming-room-joined', { socketId: socket.id, usersInRoom })
     })
 
-    socket.on('sending-signal', payload => {
-        io.to(payload.userToSignal).emit('signal-request', { signal: payload.signal, userSignaling: payload.userSignaling })
+    socket.on('outgoing-signal-request', payload => {
+        io.to(payload.userToSignal).emit('incoming-signal-request', { signal: payload.signal, userSignaling: payload.userSignaling })
     })
 
-    socket.on('returning-signal', payload => {
-        io.to(payload.userToSignal).emit('signal-returned', { signal: payload.signal, id: socket.id })
+    socket.on('outgoing-signal', payload => {
+        io.to(payload.userToSignal).emit('incoming-signal', { signal: payload.signal, id: socket.id })
     })
 
     socket.on('outgoing-refresh-request', payload => {
         io.to(payload.userToSignal).emit('incoming-refresh-request', { signal: payload.signal, id: socket.id })
     })
 
-    socket.on('sending-comment', data => {
-        io.in(data.roomId).emit('returning-comment', data)
+    socket.on('outgoing-comment', data => {
+        io.in(data.roomId).emit('incoming-comment', data)
     })
 
-    socket.on('sending-start-game', data => {
+    socket.on('outgoing-start-game', data => {
         const { userSignaling, roomId, gameData } = data
-        io.in(roomId).emit('returning-start-game', data)
-        // todo: investigate why not wokring in production
+        io.in(roomId).emit('incoming-start-game', data)
         const comment = {
             gameId: gameData.gameId,
             text: `${userSignaling.name} started the game`
@@ -108,29 +106,33 @@ io.on('connection', socket => {
             .catch((error) => console.log('error: ', error))
     })
 
-    socket.on('sending-stop-game', data => {
+    socket.on('outgoing-stop-game', data => {
         const { userSignaling, roomId, gameId } = data
-        io.in(roomId).emit('returning-stop-game', data)
+        io.in(roomId).emit('incoming-stop-game', data)
         const comment = { gameId, text: `${userSignaling.name} stopped the game` }
         axios
             .post(`${config.apiUrl}/glass-bead-game-comment`, comment)
             .catch((error) => console.log('error: ', error))
     })
 
-    socket.on('sending-save-game', data => {
-        io.in(data.roomId).emit('returning-save-game', data)
+    socket.on('outgoing-save-game', data => {
+        io.in(data.roomId).emit('incoming-save-game', data)
     })
 
-    socket.on('sending-audio-bead', data => {
-        io.in(data.roomId).emit('returning-audio-bead', data)
+    socket.on('outgoing-audio-bead', data => {
+        io.in(data.roomId).emit('incoming-audio-bead', data)
+    })
+
+    socket.on('outgoing-new-topic-image', data => {
+        io.in(data.roomId).emit('incoming-new-topic-image', data)
     })
 
     socket.on('outgoing-new-background', data => {
         io.in(data.roomId).emit('incoming-new-background', data)
     })
 
-    socket.on('stream-disconnected', data => {
-        io.in(data.roomId).emit('stream-disconnected', data)
+    socket.on('outgoing-stream-disconnected', data => {
+        io.in(data.roomId).emit('incoming-stream-disconnected', data)
     })
 
     socket.on('disconnecting', () => {
@@ -142,7 +144,7 @@ io.on('connection', socket => {
         const roomId = socketsToRooms[socket.id]
         if (rooms[roomId]) {
             const user = rooms[roomId].find(users => users.socketId === socket.id)
-            io.in(roomId).emit('user-left', user)
+            io.in(roomId).emit('incoming-user-left', user)
             rooms[roomId] = rooms[roomId].filter(users => users.socketId !== socket.id)
         }
     })
