0a8e32136264b2929efd7bcddb1a8daa50c7d5c5
jhweir <james@weco.io>
2022-07-14 16:20:18 +0100

    respond-to-weave-invite route updates

--- routes/Account.js
+++ routes/Account.js
@@ -6,8 +6,9 @@ const sequelize = require('sequelize')
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const authenticateToken = require('../middleware/authenticateToken')
-const { Holon, User, Notification, HolonUser, UserPost } = require('../models')
+const { Holon, User, Notification, HolonUser, UserPost, Post, Weave } = require('../models')
 const { totalUserPosts } = require('../GlobalConstants')
+const ScheduledTasks = require('../ScheduledTasks')
 
 // GET
 router.get('/account-data', authenticateToken, (req, res) => {
@@ -68,6 +69,17 @@ router.get('/account-notifications', authenticateToken, (req, res) => {
                 model: Holon,
                 as: 'secondarySpace',
                 attributes: ['id', 'handle', 'name', 'flagImagePath'],
+            },
+            {
+                model: Post,
+                as: 'relatedPost',
+                include: [
+                    {
+                        model: Weave,
+                        attributes: ['state'],
+                        required: false,
+                    },
+                ],
             },
         ],
     }).then((notifications) => res.send(notifications))
@@ -179,37 +191,152 @@ router.post('/respond-to-weave-invite', authenticateToken, async (req, res) => {
     const accountId = req.user.id
     const { postId, notificationId, response } = req.body
 
-    const updateUserPostState = new Promise((resolve) => {
-        UserPost.update({ state: response }, { where: { postId, userId: accountId } }).then(() => {
-            UserPost.findAll({ where: { postId, type: 'weave', relationship: 'player' } }).then(
-                async (players) => {
-                    if (players.find((p) => ['pending', 'rejected'].includes(p.state))) resolve()
-                    else {
-                        const firstPlayerId = players.find((p) => p.index === 1).userId
-                        const firstPlayer = await User.findOne({
-                            where: { id: firstPlayerId },
-                            attributes: ['email', 'name'],
-                        })
-                        const createMoveNotification = Notification.create({
+    const updateUserPostState = await UserPost.update(
+        { state: response },
+        { where: { postId, userId: accountId } }
+    )
+
+    const updateNotification = await Notification.update(
+        { state: response },
+        { where: { id: notificationId } }
+    )
+
+    const post = await Post.findOne({
+        where: { id: postId, state: 'visible' },
+        include: [
+            {
+                model: User,
+                as: 'Creator',
+                attributes: ['id', 'name', 'handle', 'email'],
+            },
+            {
+                model: Weave,
+                attributes: ['state', 'moveTimeWindow'],
+            },
+            {
+                model: User,
+                as: 'StringPlayers',
+                attributes: ['id', 'name', 'handle', 'email'],
+                through: {
+                    where: { type: 'weave' },
+                    attributes: ['index'],
+                },
+            },
+        ],
+    })
+
+    Promise.all([updateUserPostState, updateNotification]).then(async () => {
+        if (post && post.Weave.state !== 'cancelled') {
+            const players = post.StringPlayers.sort((a, b) => a.UserPost.index - b.UserPost.index)
+            const respondingPlayer = post.StringPlayers.find((p) => p.id === accountId)
+            const otherPlayers = post.StringPlayers.filter((p) => p.id !== accountId)
+            // if player rejected: update weave state, notify other players
+            if (response === 'rejected') {
+                const updateWeaveState = await Weave.update(
+                    { state: 'cancelled' },
+                    { where: { postId } }
+                )
+                const notifyOtherPlayers = await Promise.all(
+                    otherPlayers.map(
+                        (p) =>
+                            new Promise(async (resolve) => {
+                                const notifyPlayer = await Notification.create({
+                                    type: 'weave-rejected',
+                                    ownerId: p.id,
+                                    userId: accountId,
+                                    postId: postId,
+                                    seen: false,
+                                })
+                                const emailPlayer = await sgMail.send({
+                                    to: p.email,
+                                    from: {
+                                        email: 'admin@weco.io',
+                                        name: 'we { collective }',
+                                    },
+                                    subject: 'New notification',
+                                    text: `
+                                    Hi ${p.name}, ${respondingPlayer.name} has rejected their invitation to weave so the game is now cancelled.
+                                    https://${config.appURL}/p/${postId}
+                                `,
+                                    html: `
+                                    <p>
+                                        Hi ${p.name},
+                                        <br/>
+                                        <a href='${config.appURL}/u/${respondingPlayer.handle}'>${respondingPlayer.name}</a> has rejected 
+                                        their invitation to weave so <a href='${config.appURL}/p/${postId}'>the game</a> is now cancelled.
+                                    </p>
+                                `,
+                                })
+                                Promise.all([notifyPlayer, emailPlayer])
+                                    .then(() => resolve())
+                                    .catch((error) => resolve(error))
+                            })
+                    )
+                )
+                Promise.all([updateWeaveState, notifyOtherPlayers])
+                    .then(() => res.status(200).json({ message: 'Success' }))
+                    .catch((error) => console.log(error))
+            } else {
+                // if player accepted:
+                const notifyGameCreator = await Notification.create({
+                    type: 'weave-accepted',
+                    ownerId: post.Creator.id,
+                    userId: accountId,
+                    postId: postId,
+                    seen: false,
+                })
+                const emailGameCreator = await sgMail.send({
+                    to: post.Creator.email,
+                    from: {
+                        email: 'admin@weco.io',
+                        name: 'we { collective }',
+                    },
+                    subject: 'New notification',
+                    text: `
+                            Hi ${post.Creator.name}, ${respondingPlayer.name} has accepted their invitation to your Weave.
+                            https://${config.appURL}/p/${postId}
+                        `,
+                    html: `
+                        <p>
+                            Hi ${post.Creator.name},
+                            <br/>
+                            <a href='${config.appURL}/u/${respondingPlayer.handle}'>${respondingPlayer.name}</a> has accepted 
+                            their invitation to your <a href='${config.appURL}/p/${postId}'>Weave</a>.
+                        </p>
+                    `,
+                })
+                Promise.all([notifyGameCreator, emailGameCreator]).then(async () => {
+                    // if some players still pending: return
+                    if (
+                        otherPlayers.find((p) => ['pending', 'rejected'].includes(p.UserPost.state))
+                    ) {
+                        res.status(200).json({ message: 'Success' })
+                    } else {
+                        // if all players ready: update weave state and notify first player
+                        const updateWeaveState = await Weave.update(
+                            { state: 'active' },
+                            { where: { postId } }
+                        )
+                        const notifyFirstPlayer = await Notification.create({
                             type: 'weave-move',
-                            ownerId: firstPlayerId,
+                            ownerId: players[0].id,
                             postId: postId,
                             seen: false,
                         })
-                        const sendMoveEmail = sgMail.send({
-                            to: firstPlayer.email,
+                        const emailFirstPlayer = await sgMail.send({
+                            to: players[0].email,
                             from: {
                                 email: 'admin@weco.io',
                                 name: 'we { collective }',
                             },
                             subject: 'New notification',
                             text: `
-                                    Hi ${firstPlayer.name}, it's your move!
+                                    Hi ${players[0].name}, it's your move!
                                     Add a new bead to the weave on weco: https://${config.appURL}/p/${postId}
                                 `,
                             html: `
                                     <p>
-                                        Hi ${firstPlayer.name},
+                                        Hi ${players[0].name},
                                         <br/>
                                         It's your move!
                                         <br/>
@@ -217,20 +344,26 @@ router.post('/respond-to-weave-invite', authenticateToken, async (req, res) => {
                                     </p>
                                 `,
                         })
-                        Promise.all([createMoveNotification, sendMoveEmail]).then(() => resolve())
+                        const scheduleWeaveMoveJobs = post.Weave.moveTimeWindow
+                            ? ScheduledTasks.scheduleWeaveMoveJobs(
+                                  postId,
+                                  players[0],
+                                  post.Weave.moveTimeWindow
+                              )
+                            : null
+                        Promise.all([
+                            updateWeaveState,
+                            notifyFirstPlayer,
+                            emailFirstPlayer,
+                            scheduleWeaveMoveJobs,
+                        ]).then(() => res.status(200).json({ message: 'Success' }))
                     }
-                }
-            )
-        })
+                })
+            }
+        } else {
+            res.status(200).json({ message: 'Game already cancelled' })
+        }
     })
-    const updateNotification = await Notification.update(
-        { state: response },
-        { where: { id: notificationId } }
-    )
-
-    Promise.all([updateUserPostState, updateNotification]).then(
-        res.status(200).json({ message: 'Success' })
-    )
 })
 
 module.exports = router
