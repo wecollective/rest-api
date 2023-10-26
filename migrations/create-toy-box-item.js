dbb82f6d4342b0652c07f82e69f647ac8b87ffcb
jhweir <james@weco.io>
2023-10-26 21:46:22 +0100

    ToyBoxItem table set up

---
+++ migrations/create-toy-box-item.js
@@ -0,0 +1,39 @@
+'use strict'
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('ToyBoxItems', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+            },
+            row: {
+                type: Sequelize.INTEGER,
+            },
+            index: {
+                type: Sequelize.INTEGER,
+            },
+            itemType: {
+                type: Sequelize.STRING,
+            },
+            itemId: {
+                type: Sequelize.INTEGER,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        })
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('ToyBoxItems')
+    },
+}
---
+++ models/ToyBoxItem.js
@@ -0,0 +1,23 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+    const ToyBoxItem = sequelize.define(
+        'ToyBoxItem',
+        {
+            id: {
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+            },
+            userId: DataTypes.INTEGER,
+            row: DataTypes.INTEGER,
+            index: DataTypes.INTEGER,
+            itemType: DataTypes.STRING,
+            itemId: DataTypes.INTEGER,
+        },
+        {}
+    )
+    ToyBoxItem.associate = function (models) {
+        // associations can be defined here
+    }
+    return ToyBoxItem
+}
