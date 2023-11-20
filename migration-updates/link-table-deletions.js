dc62a69a5b49542534feb6f877e3cab463e778aa
jhweir <james@weco.io>
2023-11-20 14:45:07 +0000

    Link table migrations

---
+++ migration-updates/link-table-deletions.js
@@ -0,0 +1,22 @@
+// Sample migration file demonstrating the different sequilize table transactions
+// Add new file to 'migrations' folder when complete and run `npx sequelize-cli db:migrate` to migrate changes
+// Table names are pluralised versions of their respective model names
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.sequelize.transaction((t) => {
+            return Promise.all([
+                queryInterface.removeColumn('Links', 'type', { transaction: t }),
+                queryInterface.removeColumn('Links', 'index', { transaction: t }),
+            ])
+        })
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.sequelize.transaction((t) => {
+            return Promise.all([
+                // include reverse transations here to enable undo: `npx sequelize-cli db:migrate:undo:all`
+            ])
+        })
+    },
+}
